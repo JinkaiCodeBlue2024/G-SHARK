8d4db67133f2ea77bfc46b5396eaf65f7276104a
ykmt8128 <j20397ym@edu.tuis.ac.jp>
2023-10-05 19:16:15 +0900

    Change the implementation of error detection in PlantUML code.

--- backend/regenerate_network_figure.py
+++ backend/regenerate_network_figure.py
@@ -2,7 +2,6 @@ import base64
 import string
 from zlib import compress
 from typing import Tuple
-import xml.etree.ElementTree as et
 
 import requests
 
@@ -34,35 +33,28 @@ def get_svg(code: str) -> str:
     return response.content
 
 
-def check_plantuml_syntax(svg_code: str) -> Tuple[int, str]:
+def check_plantuml_syntax(plantuml_code: str) -> Tuple[int, str]:
     """
     Check the contents of the SVG file obtained from the PlantUML API and detect syntax errors.
 
     :return 1(int): no syntax errors -> 0, syntax errors -> -1
-    :return 2(str): no syntax errors -> SVG, syntax errors -> error message
+    :return 2(str): no syntax errors -> SVG data, syntax errors -> error message
     """
-    root = et.fromstring(svg_code)
-    SVG_G_TAG = "{http://www.w3.org/2000/svg}g"
-    SVG_TEXT_TAG = "{http://www.w3.org/2000/svg}text"
-    g_elems = root.find(SVG_G_TAG)
-    text_elems = g_elems.findall(SVG_TEXT_TAG)
-    text_list = [child.text for child in text_elems]
+    encoded = deflate(plantuml_code)
+    response = requests.get(PLANTUML_BASE_URL + encoded)
 
-    error_detection_string = "[From string (line"
-    error_flag = False
-    if error_detection_string in " ".join(text_list):
-        error_flag = True
-
-    if error_flag:
+    description = response.headers["X-Plantuml-Diagram-Description"]
+    if description == "(Error)":
         status = -1
-        all_text = " ".join(text_list)
-        error_message_part = all_text[all_text.index(error_detection_string):]
-        response = error_message_part.replace("\xa0", "\n")
+        error = response.headers["X-Plantuml-Diagram-Error"]
+        error_line = response.headers["X-Plantuml-Diagram-Error-Line"]
+        check_response = f"Error: {error}\nError Line: {error_line}\n"
     else:
         status = 0
-        response = svg_code
+        check_response = response.content
+    print(f"{check_response = }")
 
-    return status, response
+    return status, check_response
 
 
 def generate_figure_code(figure_system_prompt: str, figure_user_prompt: str) -> str:
@@ -75,16 +67,15 @@ def generate_figure_code(figure_system_prompt: str, figure_user_prompt: str) -> 
 
     figure_response = chat_with_gpt(figure_system_prompt, figure_user_prompt)
     print(f"{figure_response = }")
-    plantuml_response = get_svg(figure_response)
 
     for i in range(MAX_NUM_OF_REGENERATE):
-        status, check_response = check_plantuml_syntax(plantuml_response)
+        status, check_response = check_plantuml_syntax(figure_response)
         if status == 0:
             break
 
         figure_user_prompt = (
             "There was an error in this PlantUML code, resulting in a syntax error. "
-            + "Please fix it based on the hint."
+            + "Please fix it based on the hint and output only PlantUML code without code block."
             + "\n"
             + "<code>"
             + "\n"
@@ -95,7 +86,6 @@ def generate_figure_code(figure_system_prompt: str, figure_user_prompt: str) -> 
         )
 
         figure_response = chat_with_gpt(figure_system_prompt, figure_user_prompt)
-        print(f"figure_response regenerate {i} = {figure_response}")
-        plantuml_response = get_svg(figure_response)
+        print(f"(regenerate {i})figure_response = {figure_response}")
 
     return figure_response
